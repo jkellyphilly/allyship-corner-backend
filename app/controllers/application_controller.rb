fb5f4849a44b9e392b2581c7867d734937e28f19
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-02 15:59:11 -0400

    can successfully create user and show event cards with authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::API
     request.headers['Authorization']
   end
 
-  def decoded_token(token)
+  def decoded_token
     if auth_header
       token = auth_header.split(' ')[1]
       begin
