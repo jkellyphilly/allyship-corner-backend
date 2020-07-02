ef6c760e5d15f9c24a6d7ccfd689eb0166d1e9fe
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-02 17:07:01 -0400

    removed unneccessary binding.pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       @token = encode_token(user_id: @user.id)
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
-      render json: { error: @user.errors.full_messages[0] }, status: :not_acceptable
+      render json: { message: @user.errors.full_messages[0] }, status: :not_acceptable
     end
   end
 
