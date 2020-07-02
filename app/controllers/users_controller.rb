ca6af2eb960eb0df32963777c96b0b5a9b14f2af
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-02 12:41:04 -0400

    ability to create user from sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
 
   def create
     binding.pry
+    user = User.create(user_params)
+    binding.pry
+    if user.valid?
+      render json: user
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :events
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
