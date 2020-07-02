bfcd10c47f7fcc949490fd71d8db615e2e340967
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-02 11:09:59 -0400

    adding user model/controller/serializer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_200002) do
+ActiveRecord::Schema.define(version: 2020_07_02_150411) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2020_06_30_200002) do
     t.string "location"
     t.integer "attendees"
     t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
