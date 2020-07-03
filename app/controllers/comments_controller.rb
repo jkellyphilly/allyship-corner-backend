623c3a392e8463aa3fd7b19d90e00df2d063fb78
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-03 10:22:59 -0400

    adding validations for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,11 @@ class CommentsController < ApplicationController
 
   def create
     new_comment = Comment.create(comment_params)
-    render json: CommentSerializer.new(new_comment)
+    if new_comment.valid?
+      render json: CommentSerializer.new(new_comment), status: :created
+    else
+      render json: { message: new_comment.errors.full_messages[0] }, status: :not_acceptable
+    end
   end
 
   def destroy
