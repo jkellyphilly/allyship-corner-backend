502a3203ef4880ee9d2d00bc4ceadd57d0b9f482
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-07 15:25:49 -0400

    making users a local variable instead of an instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,13 @@ class UsersController < ApplicationController
     render json: UserSerializer.new(users)
   end
 
-  # TODO: make the instance variables plain variables... no need to be instance
   def create
-    @user = User.create(user_params)
-    if @user.valid?
-      @token = encode_token(user_id: @user.id)
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    user = User.create(user_params)
+    if user.valid?
+      token = encode_token(user_id: user.id)
+      render json: { user: UserSerializer.new(user), jwt: token }, status: :created
     else
-      render json: { message: @user.errors.full_messages[0] }, status: :not_acceptable
+      render json: { message: user.errors.full_messages[0] }, status: :not_acceptable
     end
   end
 
