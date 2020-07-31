5e9e6a092575675eb658fa3e69fd49b5838b619d
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-31 16:57:13 -0400

    updating CORS origin to be herokuapp

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # TODO: update this to only include the origins I want to allow
-    origins '*'
+    origins 'allyship-corner-api.herokuapp.com'
 
     resource '*',
       headers: :any,
