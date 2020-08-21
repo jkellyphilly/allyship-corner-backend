79e2e40ef8cc0339e806e8d9e9f72c9ce859576f
jkellyphilly <joel.kelly.philly@gmail.com>
2020-08-21 16:54:41 -0400

    updating Event associations to include Attendee and EventAttendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   has_many :comments
   belongs_to :user
+  has_many :event_attendees
+  has_many :attendees, through: :event_attendees
 
   validates :name, :location, :image_url, :attendees, presence: true
 end
